96dc2de134052c30a6b7ab343470c22344e7493f
MichaelArestad <marestad@gmail.com>
2015-01-25 14:46:17 -0700

    Started in on main nav styles
    https://cloudup.com/cIaU8aJT5tA

--- header.php
+++ header.php
@@ -27,6 +27,8 @@
 			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
 		</div><!-- .site-branding -->
 
+		<?php get_search_form(); ?>
+
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<button class="menu-toggle" aria-controls="menu" aria-expanded="false"><?php _e( 'Primary Menu', 'bookmarks' ); ?></button>
 			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
